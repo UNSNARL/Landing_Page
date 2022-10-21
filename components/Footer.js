72969ac80c2d1177c526d8003158d19d20276e84
Arka <arkadatta2002@gmail.com>
2022-10-21 11:30:41 +0530

    footer done

--- components/Footer.js
+++ components/Footer.js
@@ -6,13 +6,15 @@ const Footer = () => {
     <div>
       <div class="row mt-5">
         <div class="col-12 col-md-6 col-lg-3">
+        <div class={styles.heading}>
         <img className={styles.img} src="Unsnarl_WEB.svg" />
         <h1 className={styles.headingtext}>UNSNARL</h1>
         </div>
+        </div>
 
-        <div class="col-12 col-md-6 col-lg-3">
+        <div class="col-12 col-md-6 col-lg-3 text-white">
           <div className='d-flex flex-column'>
-            <h4 > Quick Link</h4>
+            <h4 className='mb-5'> Quick Link</h4>
             <h6 >Home</h6>
             <h6 >Products</h6>
             <h6 >About</h6>
@@ -21,9 +23,9 @@ const Footer = () => {
           </div>
         </div>
 
-        <div class="col-12 col-md-6 col-lg-3">
+        <div class="col-12 col-md-6 col-lg-3 text-white">
           <div className='d-flex flex-column'>
-            <h4 > Resources</h4>
+            <h4 className='mb-5'> Resources</h4>
             <h6 >Download Whitepaper</h6>
             <h6 >Smart Token</h6>
             <h6 >Blockchain Explorer</h6>
@@ -33,14 +35,34 @@ const Footer = () => {
         </div>
 
         <div class="col-12 col-md-6 col-lg-3">
-          <div className='d-flex flex-column'>
-            <h3 >We accept following</h3>
-            <h3 >payment systems</h3>
-            <div className='d-flex'>
-              <img src="Visa.png" />
-              <img src="Mastercard.png" />
-              <img src="Bitcoin.png" />
+          <div className='d-flex flex-column '>
+            <h3 className='text-white'>We accept following</h3>
+            <h3 className='text-white'>payment systems</h3>
+            <div className='d-flex mt-5 mb-5 '>
+              <img class='mr-3' src="Visa.png" />
+              <img class='mr-3' src="Mastercard.png" />
+              <img class='mr-3' src="Bitcoin.png" />
             </div>
+          </div>
+        </div>
+
+        <div class="col-12 col-md-6 mt-5">
+          <div class="d-flex flex-column">
+             <p className='text-white ml-5'>
+          ©2022 UNSNARL. All rights reserved
+             </p>
+          </div>
+        </div>
+
+        <div class="col-12 col-md-6 mb-5 mt-5">
+          <div class="d-flex flex-column ">
+            <div className='ml-auto mr-5'>
+            <img class='mr-3' src='facebook-f 1.svg' />
+            <img class='mr-3' src='instagram 1.svg' />
+            <img class='mr-3' src='linkedin 1.svg' />
+            <img class='mr-3' src='twitter 1.svg' />
+            <img class='mr-3' src='youtube 1.svg' />
+          </div>
           </div>
         </div>
 
--- components/How.js
+++ components/How.js
@@ -1,102 +1,107 @@
-import React from 'react'
-import styles from '../styles/How.module.css'
+import React from "react";
+import styles from "../styles/How.module.css";
 
 const How = () => {
   return (
     <div className={styles.container}>
-        <h1 className={styles.heading1}>How to we work</h1>
-        <h1 className={styles.heading2}>Our System and Journey</h1>
+      <h1 className={styles.heading1}>How to we work</h1>
+      <h1 className={styles.heading2}>Our System and Journey</h1>
 
-        <div class='row'>
-            <div class="col-12 col-md-6 mb-3">
-                <div>
-                    <h1 className={styles.heading3}>We build a Trusted </h1>
-                    <h1 className={styles.heading3}>community of Developers, </h1>
-                    <h1 className={styles.heading3}>Ethical Hackers and Auditors</h1>
-                    <button className={styles.button}>Learn More</button>
-                </div>
-            </div>
-            <div class="col-12 col-md-6 mb-3 ">
-                <div>
-                    <img className={styles.img} src="Chart.svg" />
-                </div>
-                <div>
-                    <img className={styles.img1} src="carbon_add.svg" />
-                </div>
-            </div>
+      <div class="row">
+        <div class="col-12 col-md-6 mb-3">
+          <div>
+            <h1 className={styles.heading3}>We build a Trusted </h1>
+            <h1 className={styles.heading3}>community of Developers, </h1>
+            <h1 className={styles.heading3}>Ethical Hackers and Auditors</h1>
+            <button className={styles.button}>Learn More</button>
+          </div>
+        </div>
+        <div class="col-12 col-md-6 mb-3 ">
+          <div>
+            <img className={styles.img} src="Chart.svg" />
+          </div>
+          <div>
+            <img className={styles.img1} src="carbon_add.svg" />
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-12 col-md-6 mb-3 ">
+          <div>
+            <img className={styles.img4} src="image21.png" />
+          </div>
         </div>
 
-        <div class='row'>
-            <div class="col-12 col-md-6 mb-3 ">
-                <div>
-                    <img className={styles.img4} src="image21.png" />
-                </div>
-            </div>
+        <div class="col-12 col-md-6 mb-3">
+          <div>
+            <h1 className={styles.heading4}>Our team of experts review all</h1>
+            <h1 className={styles.heading4}>the recent hacks/exploits and</h1>
+            <h1 className={styles.heading4}>devise healthy security </h1>
+            <h1 className={styles.heading4}>practices.</h1>
+            <button className={styles.button1}>Learn More</button>
+          </div>
+        </div>
+      </div>
 
-            <div class="col-12 col-md-6 mb-3">
-                <div>
-                    <h1 className={styles.heading4}>Our team of experts review all</h1>
-                    <h1 className={styles.heading4}>the recent hacks/exploits and</h1>
-                    <h1 className={styles.heading4}>devise healthy security </h1>
-                    <h1 className={styles.heading4}>practices.</h1>
-                    <button className={styles.button1}>Learn More</button>
-                </div>
-            </div>
+      <div class="row mt-5">
+        <div class="col-12 col-md-6 mb-3">
+          <div>
+            <h1 className={styles.heading3}>We secure web3 Enterprises </h1>
+            <h1 className={styles.heading3}>by building tools using</h1>
+            <h1 className={styles.heading3}>cutting-edge technologies.</h1>
+            <button className={styles.button}>Learn More</button>
+          </div>
+        </div>
+        <div class="col-12 col-md-6 mb-3 ">
+          <table id="customers" className="text-white">
+           <tbody>
+            <tr>
+              <td className={styles.ele}>
+                <h6>Smart Contract Auditing</h6>
+              </td>
+              <td className={styles.ele1}>Coming Soon </td>
+              <td className={styles.ele2}>Learn More</td>
+            </tr>
+            <tr className={styles.ele4}>
+              <td className={styles.ele3}>
+                <img className={styles.img2} src="detect-box.png" />
+                <h4 className={styles.ele6}>DetectBox</h4>
+                <div className={styles.ele5}>100K Users</div>
+              </td>
+              <td className={styles.ele1}>Bug Bounty</td>
+              <td className={styles.ele2}>Prices and Pricing</td>
+            </tr>
+            <tr>
+              <td className={styles.ele}>
+                <h6>Automated Anomaly Detection</h6>
+              </td>
+              <td className={styles.ele1}>Coming Soon </td>
+              <td className={styles.ele2}>Learn More</td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <div class="row mt-5">
+        <div class="col-12 col-md-6 mb-3 ">
+          <div>
+            <img className={styles.img5} src="process.png" />
+          </div>
         </div>
 
-        <div class='row mt-5'>
-            <div class="col-12 col-md-6 mb-3">
-                <div>
-                    <h1 className={styles.heading3}>We secure web3 Enterprises </h1>
-                    <h1 className={styles.heading3}>by building tools using</h1>
-                    <h1 className={styles.heading3}>cutting-edge technologies.</h1>
-                    <button className={styles.button}>Learn More</button>
-                </div>
-            </div>
-            <div class="col-12 col-md-6 mb-3 ">
-                <table id="customers" className='text-white'>
-                    <tr>
-                        <td className={styles.ele}><h6>Smart Contract Auditing</h6></td>
-                        <td className={styles.ele1}>Coming Soon </td>
-                        <td className={styles.ele2}>Learn More</td>
-                    </tr>
-                    <tr className={styles.ele4}>
-                        <td className={styles.ele3}>
-                            <img className={styles.img2} src="detect-box.png" />
-                            <h4 className={styles.ele6}>DetectBox</h4>
-                            <div className={styles.ele5}>100K Users</div>
-                        </td>
-                        <td className={styles.ele1}>Bug Bounty</td>
-                        <td className={styles.ele2}>Prices and Pricing</td>
-                    </tr>
-                    <tr>
-                        <td className={styles.ele}><h6>Automated Anomaly Detection</h6></td>     
-                        <td className={styles.ele1}>Coming Soon </td>
-                        <td className={styles.ele2}>Learn More</td>
-                    </tr>
-                </table>
-            </div>
+        <div class="col-12 col-md-6 mb-3">
+          <div>
+            <h1 className={styles.heading4}>We try to build a secure </h1>
+            <h1 className={styles.heading4}>development pipeline for web3</h1>
+            <h1 className={styles.heading4}>development. </h1>
+            <button className={styles.button1}>Learn More</button>
+          </div>
         </div>
-
-        <div class='row mt-5'>
-            <div class="col-12 col-md-6 mb-3 ">
-                <div>
-                    <img className={styles.img5} src="process.png" />
-                </div>
-            </div>
-
-            <div class="col-12 col-md-6 mb-3">
-                <div>
-                    <h1 className={styles.heading4}>We try to build a secure </h1>
-                    <h1 className={styles.heading4}>development pipeline for web3</h1>
-                    <h1 className={styles.heading4}>development. </h1>
-                    <button className={styles.button1}>Learn More</button>
-                </div>
-            </div>
-        </div>
-        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default How
+export default How;
