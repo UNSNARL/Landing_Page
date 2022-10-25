57fc2767128cc559a107050850d9884c836cad3a
Arka <arkadatta2002@gmail.com>
2022-10-25 17:21:26 +0530

    async

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400&display=swap"
           rel="stylesheet"
         />
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous" async />
       </Head>
 
       <main className={styles.main}>
